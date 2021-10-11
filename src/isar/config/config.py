6251a9da5257988433fc6e2dd1f74829e82ba8f3
Edda Solem <esole@equinor.com>
2021-10-11 13:29:32 +0200

    Use importlib for importing config

--- src/isar/config/config.py
+++ src/isar/config/config.py
@@ -1,4 +1,5 @@
 import logging
+import importlib.resources as pkg_resources
 from configparser import ConfigParser
 from os import getenv
 from pathlib import Path
@@ -21,26 +22,12 @@ class Config(object):
 
         self.parser = ConfigParser()
 
-        cwd: Path = Path.cwd()
-        default_config: Path
-        env_config: Path
+        with pkg_resources.path("isar.config", "default.ini") as filepath:
+            found_default: bool = self.parser.read(filepath)
 
-        # The Azure Pipelines job is unable to import with the same script as it starts to search in site-packages.
-        # This if-statement is a workaround to enable running locally, from a robot package and in the pipeline.
-        if str(cwd) == "/home/vsts/work/1/s":
-            print("Running in Azure Pipelines")
-            default_config: Path = Path("./src/isar/config/default.ini")
-            env_config: Path = Path(f"./src/isar/config/{env}.ini")
-        else:
-            print("Running somewhere else")
-            default_config: Path = Path(
-                f"{Path(__file__).parent.resolve()}/default.ini"
-            )
-            env_config: Path = Path(f"{Path(__file__).parent.resolve()}/{env}.ini")
+        with pkg_resources.path("isar.config", f"{env}.ini") as filepath:
+            found_env: bool = self.parser.read(filepath)
 
-        found_default: bool = self.parser.read(default_config)
-        found_env: bool = self.parser.read(env_config)
-        print(f"Default config: {default_config}")
         if not found_default or not found_env:
             raise ConfigurationError(
                 f"Failed to import configuration, default: {found_default}, env: {found_env}"
