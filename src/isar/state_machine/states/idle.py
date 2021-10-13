778d0fd83529abd4399df7415568dcc787d7afe8
Anette Uttisrud <autt@equinor.com>
2021-10-13 15:56:00 +0200

    Remove self transistions from Idle

--- src/isar/state_machine/states/idle.py
+++ src/isar/state_machine/states/idle.py
@@ -1,5 +1,6 @@
 import logging
 import time
+from os import stat_result
 from typing import TYPE_CHECKING
 
 from transitions import State
@@ -12,23 +13,34 @@ if TYPE_CHECKING:
 
 class Idle(State):
     def __init__(self, state_machine: "StateMachine"):
-        super().__init__(name="idle", on_enter=self.start)
+        super().__init__(name="idle", on_enter=self.start, on_exit=self.stop)
         self.state_machine: "StateMachine" = state_machine
-        self.i = 0
         self.logger = logging.getLogger("state_machine")
 
     def start(self):
         time.sleep(self.state_machine.sleep_time)
-        if self.state_machine.should_stop():
-            self.state_machine.stop_mission()
 
         self.state_machine.update_status()
         self.logger.info(f"State: {self.state_machine.status.current_state}")
 
-        next_state: States = self.get_next_state()
+        self._run()
+
+    def stop(self):
+        pass
+
+    def _run(self):
+        while True:
+            time.sleep(self.state_machine.sleep_time)
+            if self.state_machine.should_stop():
+                self.state_machine.stop_mission()
+
+            next_state: States = self._get_next_state()
+            if not next_state == States.Idle:
+                break
+
         self.state_machine.to_next_state(next_state)
 
-    def get_next_state(self) -> States:
+    def _get_next_state(self) -> States:
         (should_start_mission, mission) = self.state_machine.should_start_mission()
         if should_start_mission:
             self.state_machine.start_mission(mission)
