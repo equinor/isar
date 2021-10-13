53ced0521a7a42a51da7c15c47b67f7ed1057000
Anette Uttisrud <autt@equinor.com>
2021-10-13 15:57:25 +0200

    Remove self transistions from Send
    A new integration test that address the case where the send state is
    unsuccessful and then successful before running of out retries replaces
    the now outdated unit tests for the send state.

--- src/isar/state_machine/states/send.py
+++ src/isar/state_machine/states/send.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
 
 class Send(State):
     def __init__(self, state_machine: "StateMachine"):
-        super().__init__(name="send", on_enter=self.start)
+        super().__init__(name="send", on_enter=self.start, on_exit=self.stop)
         self.state_machine: "StateMachine" = state_machine
         self.send_failure_counter = 0
         self.send_failure_counter_limit = 10
@@ -22,24 +22,52 @@ class Send(State):
         self.state_machine.update_status()
         self.logger.info(f"State: {self.state_machine.status.current_state}")
 
-        if self.state_machine.status.mission_schedule.mission_steps:
-            self.state_machine.status.current_mission_step = self.get_current_mission()
+        self._run()
+
+    def stop(self):
+        self.send_failure_counter = 0
+
+    def _run(self):
+        while True:
             if self.state_machine.should_stop():
                 self.state_machine.stop_mission()
             if not self.state_machine.status.mission_in_progress:
-                self.state_machine.to_next_state(States.Cancel)
+                next_state: States = States.Cancel
+                break
 
-            next_action = self.send_mission(
+            if not self.state_machine.status.mission_schedule.mission_steps:
+                next_state: States = States.Cancel
+                break
+
+            self.state_machine.status.current_mission_step = self._get_current_mission()
+            next_state: States = self._send_mission(
                 self.state_machine.status.current_mission_step
             )
-            self.state_machine.to_next_state(next_action)
-        else:
-            self.state_machine.to_next_state(States.Cancel)
+            if not next_state == States.Send:
+                break
 
-    def get_current_mission(self) -> Step:
+            if self.state_machine.robot.mission_scheduled():
+                next_state: States = States.Monitor
+                break
+
+            self.send_failure_counter += 1
+            self.logger.info("sending failed #: " + str(self.send_failure_counter))
+            if self.send_failure_counter >= self.send_failure_counter_limit:
+                self.logger.error(
+                    f"Failed to send mission after {self.send_failure_counter_limit} attempts. Cancelling mission."
+                )
+                next_state: States = States.Cancel
+                break
+
+            self.state_machine.status.mission_schedule.mission_steps.insert(
+                0, self.state_machine.status.current_mission_step  # type: ignore
+            )
+        self.state_machine.to_next_state(next_state)
+
+    def _get_current_mission(self) -> Step:
         return self.state_machine.status.mission_schedule.mission_steps.pop(0)
 
-    def send_mission(self, current_mission_step: Step) -> States:
+    def _send_mission(self, current_mission_step: Step) -> States:
         (
             send_success,
             mission_instance_id,
@@ -64,24 +92,4 @@ class Send(State):
         if send_success:
             return States.Monitor
         else:
-            return self.handle_send_failure(
-                self.state_machine.status.current_mission_step
-            )
-
-    def handle_send_failure(self, current_mission_step: Step) -> States:
-        self.logger.info("sending failed #: " + str(self.send_failure_counter + 1))
-        if not self.state_machine.robot.mission_scheduled():
-            self.send_failure_counter += 1
-            if self.send_failure_counter >= self.send_failure_counter_limit:
-                self.logger.error(
-                    f"Failed to send mission after {self.send_failure_counter_limit} attempts. Cancelling mission."
-                )
-                return States.Cancel
-            else:
-                self.state_machine.status.mission_schedule.mission_steps.insert(
-                    0, current_mission_step  # type: ignore
-                )
-                return States.Send
-        else:
-            self.send_failure_counter = 0
-            return States.Monitor
+            return States.Send
