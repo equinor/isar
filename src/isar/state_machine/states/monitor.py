cf965f56ed2c1aecb25eeb89e39d210e3fabf43d
Anette Uttisrud <autt@equinor.com>
2021-10-14 09:22:29 +0200

    Remove self transitions from Monitor

--- src/isar/state_machine/states/monitor.py
+++ src/isar/state_machine/states/monitor.py
@@ -15,7 +15,7 @@ if TYPE_CHECKING:
 class Monitor(State):
     @inject
     def __init__(self, state_machine: "StateMachine"):
-        super().__init__(name="monitor", on_enter=self.start)
+        super().__init__(name="monitor", on_enter=self.start, on_exit=self.stop)
         self.state_machine: "StateMachine" = state_machine
         self.logger = logging.getLogger("state_machine")
 
@@ -26,42 +26,47 @@ class Monitor(State):
         self.state_machine.update_status()
         self.logger.info(f"State: {self.state_machine.status.current_state}")
 
-        next_state = self.monitor_mission()
+        self._run()
+
+    def stop(self):
+        self.iteration_counter = 0
+
+    def _run(self):
+        while True:
+            if self.state_machine.should_stop():
+                self.state_machine.stop_mission()
+
+            if not self.state_machine.status.mission_in_progress:
+                next_state = States.Cancel
+                break
+
+            mission_status: MissionStatus = self.state_machine.robot.mission_status(
+                self.state_machine.status.current_mission_instance_id
+            )
+            self.state_machine.status.mission_status = mission_status
+            self._log_status(mission_status=mission_status)
+
+            if self._mission_finished(
+                mission_status=mission_status,
+                instance_id=self.state_machine.status.current_mission_instance_id,
+            ):
+                if isinstance(
+                    self.state_machine.status.current_mission_step, DriveToPose
+                ):
+                    next_state = States.Send
+                else:
+                    next_state = States.Collect
+                break
+
+            if self.state_machine.should_send_status():
+                self.state_machine.send_status()
+            time.sleep(self.state_machine.sleep_time)
+
         self.state_machine.to_next_state(next_state)
 
-    def monitor_mission(self) -> States:
-        if self.state_machine.should_stop():
-            self.iteration_counter = 0
-            self.state_machine.stop_mission()
-
-        if not self.state_machine.status.mission_in_progress:
-            self.iteration_counter = 0
-            return States.Cancel
-
-        mission_status: MissionStatus = self.state_machine.robot.mission_status(
-            self.state_machine.status.current_mission_instance_id
-        )
-        self.state_machine.status.mission_status = mission_status
-
-        self.log_status(mission_status=mission_status)
-
-        if self.mission_finished(
-            mission_status=mission_status,
-            instance_id=self.state_machine.status.current_mission_instance_id,
-        ):
-            self.iteration_counter = 0
-            if isinstance(self.state_machine.status.current_mission_step, DriveToPose):
-                return States.Send
-            return States.Collect
-
-        if self.state_machine.should_send_status():
-            self.state_machine.send_status()
-
-        time.sleep(self.state_machine.sleep_time)
-
-        return States.Monitor
-
-    def mission_finished(self, mission_status: MissionStatus, instance_id: int) -> bool:
+    def _mission_finished(
+        self, mission_status: MissionStatus, instance_id: int
+    ) -> bool:
         if mission_status == MissionStatus.Unexpected:
             self.logger.error(
                 f"Mission status on step {instance_id} returned and unexpected status string"
@@ -73,7 +78,7 @@ class Monitor(State):
             return True
         return False
 
-    def log_status(self, mission_status: MissionStatus):
+    def _log_status(self, mission_status: MissionStatus):
         if self.iteration_counter % self.log_interval == 0:
             self.state_machine.robot.log_status(
                 mission_id=self.state_machine.status.current_mission_instance_id,
