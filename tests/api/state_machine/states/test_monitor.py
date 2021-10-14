2da382f15ae0bd506378a58ea6c49d84ee1e73ef
Anette Uttisrud <autt@equinor.com>
2021-10-14 09:22:29 +0200

    Remove self transitions from Monitor

--- tests/api/state_machine/states/test_monitor.py
+++ tests/api/state_machine/states/test_monitor.py
@@ -8,45 +8,6 @@ from tests.test_utilities.mock_models.mock_step import MockStep
 
 
 @pytest.mark.parametrize(
-    "mission_in_progress, mock_mission_status, mock_mission_finished, expected_state",
-    [
-        (False, MissionStatus.Completed, True, States.Cancel),
-        (True, MissionStatus.Completed, True, States.Send),
-        (True, MissionStatus.InProgress, False, States.Monitor),
-    ],
-)
-def test_monitor_mission(
-    monitor,
-    mocker,
-    mission_in_progress,
-    mock_mission_status,
-    mock_mission_finished,
-    expected_state,
-):
-    mocker.patch.object(
-        MockRobot,
-        "mission_status",
-        return_value=mock_mission_status,
-    )
-    mocker.patch.object(
-        Monitor,
-        "mission_finished",
-        return_value=mock_mission_finished,
-    )
-    mocker.patch.object(
-        Monitor,
-        "log_status",
-        return_value=None,
-    )
-
-    monitor.state_machine.status.mission_in_progress = mission_in_progress
-    monitor.state_machine.status.current_mission_step = MockStep.drive_to()
-    next_state = monitor.monitor_mission()
-
-    assert next_state is expected_state
-
-
-@pytest.mark.parametrize(
     "mission_instance_id, mock_status, expected_output",
     [
         (1, MissionStatus.Completed, True),
@@ -58,8 +19,8 @@ def test_monitor_mission(
 def test_mission_finished(
     monitor, mocker, mission_instance_id, mock_status, expected_output
 ):
-    is_mission_finished: bool = monitor.mission_finished(
+    mission_finished: bool = monitor._mission_finished(
         mission_status=mock_status, instance_id=mission_instance_id
     )
 
-    assert is_mission_finished == expected_output
+    assert mission_finished == expected_output
