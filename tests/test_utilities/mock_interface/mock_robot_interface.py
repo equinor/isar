5372c66c18dc56d3f98d16f577049e25cfe50a06
Anette Uttisrud <autt@equinor.com>
2021-10-14 09:58:44 +0200

    Remove logging from MockRobot

--- tests/test_utilities/mock_interface/mock_robot_interface.py
+++ tests/test_utilities/mock_interface/mock_robot_interface.py
@@ -33,7 +33,6 @@ class MockRobot(RobotInterface):
             frame=Frame.Robot,
         ),
     ):
-        self.logger: Logger = logging.getLogger()
         self.schedule_step_return_value: Tuple[
             bool, Optional[Any], Optional[Joints]
         ] = schedule_step
@@ -43,35 +42,28 @@ class MockRobot(RobotInterface):
         self.robot_pose_return_value: Pose = pose
 
     def schedule_step(self, step: Step) -> Tuple[bool, Optional[Any], Optional[Joints]]:
-        self.logger.info("Mock for schedule_step in scheduler was called")
         return self.schedule_step_return_value
 
     def mission_scheduled(self) -> bool:
-        self.logger.info("Mock for mission_scheduled in scheduler was called")
-
         return self.mission_scheduled_return_value
 
     def mission_status(self, mission_id: Any) -> MissionStatus:
-        self.logger.info("Mock for mission_status in scheduler was called")
         return self.mission_status_return_value
 
     def abort_mission(self) -> bool:
-        self.logger.info("Mock for abort_mission in scheduler was called")
         return self.abort_mission_return_value
 
     def log_status(
         self, mission_id: Any, mission_status: MissionStatus, current_step: Step
     ):
-        self.logger.info("Mock for log_status in scheduler was called")
+        pass
 
     def get_inspection_references(
         self, vendor_mission_id: Any, current_step: Step
     ) -> List[Inspection]:
-        self.logger.info("Mock for get_inspection_references in storage was called")
         return [ImageReference(vendor_mission_id, mock_image_metadata())]
 
     def download_inspection_result(self, inspection: Inspection) -> InspectionResult:
-        self.logger.info("Mock for download_inspection_result in storage was called")
         return Image(
             inspection.id,
             mock_image_metadata(),
@@ -79,7 +71,6 @@ class MockRobot(RobotInterface):
         )
 
     def robot_pose(self) -> Pose:
-        self.logger.info("Mock for robot_pose in Telemetry was called")
         return self.robot_pose_return_value
 
 
