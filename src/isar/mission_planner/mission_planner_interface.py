08c0e86bf53a323a229419f1900a4b56124b0ada
aestene <arnts@equinor.com>
2021-10-15 16:20:58 +0200

    Add docstring for mission_planner_interface

--- src/isar/mission_planner/mission_planner_interface.py
+++ src/isar/mission_planner/mission_planner_interface.py
@@ -6,6 +6,15 @@ from isar.models.mission import Mission
 class MissionPlannerInterface(metaclass=ABCMeta):
     @abstractmethod
     def get_mission(self, mission_id: int) -> Mission:
+        """
+        Parameters
+        ----------
+        mission_id : int
+
+        Returns
+        -------
+        mission : Mission
+        """
         pass
 
 
