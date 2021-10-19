7bd3569a7dab6e473671ba5f73e5fac433443102
Sondre Olsen <sondre.olsen97@gmail.com>
2021-10-19 14:46:50 +0200

    Move initial module configuration into modules.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+from logging import Logger
 from threading import Thread
 from typing import List
 
@@ -5,31 +7,12 @@ from injector import Injector, Module
 
 from isar import create_app
 from isar.config import config
-from isar.modules import modules
+from isar.modules import get_injector_modules
 from isar.state_machine.state_machine import main
-
-
-def get_injector_modules() -> List[Module]:
-    injector_modules: List[Module] = []
-
-    module_config: dict = dict(config.items("modules"))
-
-    for module_key, module in modules.items():
-
-        injector_modules.append(
-            module[
-                "default"
-                if not module_key in module_config
-                else module_config[module_key]
-            ]
-        )
-
-    return injector_modules
-
 
 if __name__ == "__main__":
 
-    injector_modules: List[Module] = get_injector_modules()
+    injector_modules, module_config_keys = get_injector_modules()
     injector: Injector = Injector(injector_modules)
 
     state_machine_thread: Thread = Thread(target=main, args=[injector])
@@ -39,5 +22,10 @@ if __name__ == "__main__":
 
     host = config.get("environment", "flask_run_host")
     port = config.getint("environment", "flask_run_port")
+
+    logger: Logger = logging.getLogger("api")
+
+    module_config_log = "\n".join(module_config_keys)
+    logger.info(f"Loaded the following module configurations:\n{module_config_log}")
 
     app.run(host, port, use_reloader=False)
