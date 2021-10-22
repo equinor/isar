61f6e1e36aab3fd796914011cae7f32877fc7438
Edda Solem <esole@equinor.com>
2021-10-22 09:30:36 +0200

    Add interface test

---
+++ src/robot_interface/test_robot_interface.py
@@ -0,0 +1,19 @@
+from robot_interface.robot_interface import RobotInterface
+from typing import Tuple, List
+
+
+def interface_test(Robot: RobotInterface) -> None:
+    for func in dir(RobotInterface):
+        if not callable(getattr(Robot, func)) or func.startswith("__"):
+            continue
+
+        arg_robot: int = getattr(Robot, func).__code__.co_argcount
+        arg_isar: int = getattr(RobotInterface, func).__code__.co_argcount
+
+        args_robot: Tuple[str] = getattr(Robot, func).__code__.co_varnames[:arg_robot]
+        args_isar: Tuple[str] = getattr(RobotInterface, func).__code__.co_varnames[
+            :arg_isar
+        ]
+
+        assert arg_robot == arg_isar
+        assert args_robot == args_isar
