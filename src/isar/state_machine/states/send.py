a37571d4591df2a44177d3739e10bce8968d76c5
Vetle Koppergård <vetk@equinor.com>
2021-11-04 14:49:22 +0000

    Run seschedule_step in separate thread

--- src/isar/state_machine/states/send.py
+++ src/isar/state_machine/states/send.py
@@ -1,8 +1,14 @@
 import logging
+import time
 from typing import TYPE_CHECKING
 
 from transitions import State
 
+from isar.services.utilities.threaded_request import (
+    ThreadedRequest,
+    ThreadedRequestNotFinishedError,
+    ThreadedRequestUnexpectedError,
+)
 from isar.state_machine.states_enum import States
 from robot_interface.models.mission import Step, TakeImage
 
@@ -18,6 +24,8 @@ class Send(State):
         self.send_failure_counter_limit = 10
         self.logger = logging.getLogger("state_machine")
 
+        self.send_thread = None
+
     def start(self):
         self.state_machine.update_status()
         self.logger.info(f"State: {self.state_machine.status.current_state}")
@@ -26,12 +34,14 @@ class Send(State):
 
     def stop(self):
         self.send_failure_counter = 0
+        if self.send_thread:
+            self.send_thread.wait_for_thread()
+        self.send_thread = None
 
     def _run(self):
         while True:
             if self.state_machine.should_stop():
                 self.state_machine.stop_mission()
-            if not self.state_machine.status.mission_in_progress:
                 next_state: States = States.Cancel
                 break
 
@@ -39,57 +49,61 @@ class Send(State):
                 next_state: States = States.Cancel
                 break
 
-            self.state_machine.status.current_mission_step = self._get_current_mission()
-            next_state: States = self._send_mission(
-                self.state_machine.status.current_mission_step
-            )
-            if not next_state == States.Send:
-                break
+            if self.state_machine.should_send_status():
+                self.state_machine.send_status()
 
-            if self.state_machine.robot.mission_scheduled():
-                next_state: States = States.Monitor
-                break
-
-            self.send_failure_counter += 1
-            self.logger.info("sending failed #: " + str(self.send_failure_counter))
-            if self.send_failure_counter >= self.send_failure_counter_limit:
-                self.logger.error(
-                    f"Failed to send mission after {self.send_failure_counter_limit} attempts. Cancelling mission."
+            if not self.send_thread:
+                self.state_machine.status.current_mission_step = (
+                    self._get_current_mission()
                 )
-                next_state: States = States.Cancel
-                break
+                self.send_thread = ThreadedRequest(
+                    self.state_machine.robot.schedule_step
+                )
+                self.send_thread.start_thread(
+                    self.state_machine.status.current_mission_step
+                )
+            try:
+                (
+                    send_success,
+                    mission_instance_id,
+                    computed_joints,
+                ) = self.send_thread.get_output()
+            except ThreadedRequestNotFinishedError:
+                time.sleep(self.state_machine.sleep_time)
+                continue
+            except ThreadedRequestUnexpectedError:
+                send_success = False
+                mission_instance_id = None
+                computed_joints = None
 
-            self.state_machine.status.mission_schedule.mission_steps.insert(
-                0, self.state_machine.status.current_mission_step  # type: ignore
-            )
+            if send_success:
+                self.state_machine.status.current_mission_instance_id = (
+                    mission_instance_id
+                )
+                if isinstance(
+                    self.state_machine.status.current_mission_step,
+                    TakeImage,
+                ):
+                    self.state_machine.status.current_mission_step.computed_joints = (
+                        computed_joints
+                    )
+                self.state_machine.status.mission_schedule.mission_steps.pop(0)
+                next_state = States.Monitor
+                break
+            else:
+                self.send_failure_counter += 1
+                self.logger.info("sending failed #: " + str(self.send_failure_counter))
+                if self.send_failure_counter >= self.send_failure_counter_limit:
+                    self.logger.error(
+                        f"Failed to send mission after "
+                        f"{self.send_failure_counter_limit} attempts. "
+                        f"Cancelling mission."
+                    )
+                    next_state: States = States.Cancel
+                    break
+                self.send_thread = None
+
         self.state_machine.to_next_state(next_state)
 
     def _get_current_mission(self) -> Step:
-        return self.state_machine.status.mission_schedule.mission_steps.pop(0)
-
-    def _send_mission(self, current_mission_step: Step) -> States:
-        (
-            send_success,
-            mission_instance_id,
-            computed_joints,
-        ) = self.state_machine.robot.schedule_step(current_mission_step)
-
-        if send_success:
-            self.state_machine.status.current_mission_instance_id = mission_instance_id
-            if isinstance(
-                self.state_machine.status.current_mission_step,
-                TakeImage,
-            ):
-                self.state_machine.status.current_mission_step.computed_joints = (
-                    computed_joints
-                )
-        else:
-            send_success = False
-
-        if self.state_machine.should_send_status():
-            self.state_machine.send_status()
-
-        if send_success:
-            return States.Monitor
-        else:
-            return States.Send
+        return self.state_machine.status.mission_schedule.mission_steps[0]
