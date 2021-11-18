24370bd41b9e6f89baed8716146e7d4f6229fac4
Kristoffer Nordvik <knord@equinor.com>
2021-11-18 16:20:48 +0100

    Implement AAD Authentication

--- src/isar/apis/security/authentication.py
+++ src/isar/apis/security/authentication.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import Optional, Union
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security.base import SecurityBase
@@ -7,33 +7,18 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from injector import inject
 from jose import jwt
 from passlib.context import CryptContext
-from pydantic import BaseModel
+from pydantic import BaseModel, BaseSettings, Field, AnyHttpUrl
+
 
 from isar.config import config
 from isar.config.keyvault.keyvault_service import Keyvault
+
+from fastapi_azure_auth import SingleTenantAzureAuthorizationCodeBearer
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
-
-    @staticmethod
-    def get_token():
-        should_authenticate = config.getboolean("fastapi", "authentication_enabled")
-        if should_authenticate:
-            return OAuth2PasswordBearer(tokenUrl="token")
-        return NoSecurity
-
-
-class User(BaseModel):
-    username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    disabled: Optional[bool] = None
-
-
-class UserInDB(User):
-    hashed_password: str
 
 
 class NoSecurity(SecurityBase):
@@ -42,71 +27,44 @@ class NoSecurity(SecurityBase):
 
 
 class Authenticator:
-    @inject
     def __init__(
         self,
-        keyvault: Keyvault,
-        username=config.get("fastapi", "username"),
-        token_algorithm=config.get("fastapi", "token_algorithm"),
-        token_expire_minutes=config.getint("fastapi", "token_expire_minutes"),
+        host: str = config.get("fastapi", "run_host"),
+        port: int = config.getint("fastapi", "run_port"),
+        openapi_client_id: str = config.get("fastapi", "openapi_client_id"),
+        authentication_enabled: bool = config.getboolean(
+            "fastapi", "authentication_enabled"
+        ),
     ) -> None:
-        self.keyvault = keyvault
-        self.token_key = self.keyvault.get_secret("ISAR-API-TOKEN-KEY").value
-        self.hashed_password = self.keyvault.get_secret(
-            "ISAR-API-HASHED-PASSWORD"
-        ).value
+        self.backend_cors_origins: list[Union[str, AnyHttpUrl]] = [
+            f"http://{host}:{port}"
+        ]
+        self.openapi_client_id: str = openapi_client_id
+        self.authentication_enabled: bool = authentication_enabled
 
-        self.user_data = {
-            username: {"username": username, "hashed_password": self.hashed_password}
-        }
+    @staticmethod
+    def get_azure_scheme():
+        app_client_id = config.get("fastapi", "app_client_id")
+        return SingleTenantAzureAuthorizationCodeBearer(
+            app_client_id=app_client_id,
+            tenant_id=config.get("environment", "azure_tenant_id"),
+            scopes={
+                f"api://{app_client_id}/user_impersonation": "user_impersonation",
+            },
+        )
 
-        self.token_algorithm = token_algorithm
-        self.token_expire_minutes = token_expire_minutes
-        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    @classmethod
+    def get_scheme(cls):
+        authentication_enabled = config.getboolean("fastapi", "authentication_enabled")
+        if authentication_enabled:
+            return cls.get_azure_scheme()
+        return NoSecurity
 
-    def verify_password(self, plain_password, hashed_password):
-        return self.pwd_context.verify(plain_password, hashed_password)
-
-    def get_user(self, username: str):
-        if username in self.user_data:
-            user_dict = self.user_data[username]
-            return UserInDB(**user_dict)
-
-    def authenticate_user(self, username: str, password: str):
-        user = self.get_user(username)
-        if not user:
-            return False
-        if not self.verify_password(password, user.hashed_password):
-            return False
-        return user
-
-    def create_access_token(
-        self, data: dict, expires_delta: Optional[timedelta] = None
-    ):
-        to_encode = data.copy()
-        if expires_delta:
-            expire = datetime.utcnow() + expires_delta
+    async def load_config(self):
+        """
+        Load OpenID config on startup.
+        """
+        if self.authentication_enabled:
+            await self.get_azure_scheme().openid_config.load_config()
         else:
-            expire = datetime.utcnow() + timedelta(minutes=15)
-        to_encode.update({"exp": expire})
-        encoded_jwt = jwt.encode(
-            to_encode, key=self.token_key, algorithm=self.token_algorithm
-        )
-        return encoded_jwt
-
-    def login_for_access_token(
-        self,
-        form_data: OAuth2PasswordRequestForm = Depends(),
-    ):
-        user = self.authenticate_user(form_data.username, form_data.password)
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Incorrect username or password",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-        access_token_expires = timedelta(minutes=self.token_expire_minutes)
-        access_token = self.create_access_token(
-            data={"sub": user.username}, expires_delta=access_token_expires
-        )
-        return {"access_token": access_token, "token_type": "bearer"}
+            pass
--- src/isar/app.py
+++ src/isar/app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
 from injector import Injector
-
+from fastapi.middleware.cors import CORSMiddleware
 from isar.apis.schedule.router import create_scheduler_router
-from isar.apis.security.security_router import create_security_router
+from isar.apis.security.authentication import Authenticator
 
 
 def create_app(injector: Injector, authentication_enabled: bool = False):
@@ -15,9 +15,28 @@ def create_app(injector: Injector, authentication_enabled: bool = False):
         }
     ]
 
-    app = FastAPI(openapi_tags=tags_metadata)
+    authenticator = Authenticator()
+
+    app = FastAPI(
+        openapi_tags=tags_metadata,
+        on_startup=[authenticator.load_config],
+        swagger_ui_oauth2_redirect_url="/oauth2-redirect",
+        swagger_ui_init_oauth={
+            "usePkceWithAuthorizationCodeGrant": True,
+            "clientId": authenticator.openapi_client_id,
+        },
+    )
+
+    if authentication_enabled:
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=[
+                str(origin) for origin in authenticator.backend_cors_origins
+            ],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
     app.include_router(router=create_scheduler_router(injector=injector))
-    if authentication_enabled:
-        app.include_router(router=create_security_router(injector=injector))
     return app
