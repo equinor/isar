435cb28045cc9d3a6b9d6d4ac45e48683b1291f1
aestene <arnts@equinor.com>
2021-11-30 13:16:18 +0100

    Set release 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description="Integration and Supervisory control of Autonomous Robots",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    version="1.1.0",
+    version="1.2.0",
     author="Equinor ASA",
     author_email="fg_robots_dev@equinor.com",
     url="https://github.com/equinor/isar",
