44a46ffd795dcb9d69ae9bd11c80d9bf2c33df1d
Thomas Sundvoll <thomas@sundvoll.net>
2022-04-11 17:20:10 +0200

    Simplify mocks
    Let the default instances used in tests be defined as attributes
    to mock classes. This simplifies the mocks and are also sufficient
    for how the mocks are currently used.

--- tests/isar/state_machine/states/test_monitor.py
+++ tests/isar/state_machine/states/test_monitor.py
@@ -15,7 +15,7 @@ from tests.mocks.task import MockTask
     ],
 )
 def test_task_finished(monitor: Monitor, mock_status, expected_output):
-    task: Task = MockTask.drive_to()
+    task: Task = MockTask.drive_to
     task.status = mock_status
     task_completed: bool = monitor._task_finished(
         task=task,
@@ -34,7 +34,7 @@ def test_task_finished(monitor: Monitor, mock_status, expected_output):
 def test_should_only_upload_if_status_is_completed(
     monitor: Monitor, mock_status, should_queue_upload
 ):
-    task: Task = MockTask.take_image_in_coordinate_direction()
+    task: Task = MockTask.take_image_in_coordinate_direction
     task.status = mock_status
     monitor._process_finished_task(task)
     assert monitor.state_machine.queues.upload_queue.empty() == (
