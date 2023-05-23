0089740e5f659f8d201eb0a7b280b8822ae3e2ea
andchiind <aindreboe@gmail.com>
2023-05-23 13:44:50 +0200

    Add mission argument to upload function in test

--- src/isar/state_machine/state_machine.py
+++ src/isar/state_machine/state_machine.py
@@ -428,6 +428,7 @@ class StateMachine(object):
         self.publish_state()
 
     def reset_state_machine(self) -> None:
+        self.logger.info("Finalising state machine")
         self.stopped = False
         self.current_step = None
         self.current_task = None
