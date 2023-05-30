db9e959edc93ec15ba98ed59754acffc1774ea5b
aestene <arnts@equinor.com>
2023-05-30 07:37:13 +0200

    Catch robot exceptions when gathering telemetry

--- src/robot_interface/telemetry/mqtt_client.py
+++ src/robot_interface/telemetry/mqtt_client.py
@@ -3,9 +3,7 @@ from abc import ABCMeta, abstractmethod
 from queue import Queue
 from typing import Callable, Tuple
 
-from robot_interface.models.exceptions.robot_exceptions import (
-    RobotInvalidTelemetryException,
-)
+from robot_interface.models.exceptions import RobotException
 
 
 class MqttClientInterface(metaclass=ABCMeta):
@@ -66,7 +64,7 @@ class MqttTelemetryPublisher(MqttClientInterface):
                 payload: str = self.telemetry_method(
                     isar_id=isar_id, robot_name=robot_name
                 )
-            except RobotInvalidTelemetryException:
+            except RobotException:
                 continue
 
             self.publish(
