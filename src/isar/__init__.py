0b7d052a6798240aeacd6255cb89c1ded96627b6
JPI <joind@equinor.com>
2023-11-24 10:53:56 +0100

    Delete setup.py
    Setup.py is no longer needed, as we use pyproject isntead now.

--- src/isar/__init__.py
+++ src/isar/__init__.py
@@ -1,6 +1,1 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    pass  # package is not installed
+__version__ = "1.16.8"
