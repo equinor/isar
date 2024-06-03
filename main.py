89f70fa3ff29a2fe6208d95c8e7d9a2fffe176ae
Thomas Sundvoll <thomas@sundvoll.net>
2024-06-03 21:50:08 +0200

    Reintroduce main.py for backwards compability
    this can be removed in next minor bump of version (1.20.0)

---
+++ main.py
@@ -0,0 +1,7 @@
+# This file is added for backwards-compability for versions 1.19.2 and older.
+# It can be removed in the next minor bump (1.20.0)
+
+from isar.script import start
+
+if __name__ == "__main__":
+    start()
