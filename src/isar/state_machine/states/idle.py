f8622de8e907c24d0d4a5c257ecb5df8bbf813c4
oysand <outb@equinor.com>
2024-11-07 12:24:16 +0100

    Remove unused comma

--- src/isar/state_machine/states/idle.py
+++ src/isar/state_machine/states/idle.py
@@ -71,7 +71,7 @@ class Idle(State):
                 time.sleep(self.state_machine.sleep_time)
                 continue
 
-            except (RobotException,) as e:
+            except RobotException as e:
                 self.logger.error(
                     f"Failed to get robot status because: {e.error_description}"
                 )
