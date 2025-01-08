59c5ae38767b2ffa0f7696590a228e9f14e85bce
Anders Chirico Indrebø <ACIN@equinor.com>
2025-01-08 10:03:34 +0100

    Move transitions to separate files

---
+++ src/isar/state_machine/transitions/resume.py
@@ -0,0 +1,25 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from isar.state_machine.state_machine import StateMachine
+
+from isar.apis.models.models import ControlMissionResponse
+from robot_interface.models.mission.status import MissionStatus, TaskStatus
+
+
+def resume_mission(state_machine: "StateMachine") -> bool:
+    state_machine.logger.info(f"Resuming mission: {state_machine.current_mission.id}")
+    state_machine.current_mission.status = MissionStatus.InProgress
+    state_machine.current_mission.error_message = None
+    state_machine.current_task.status = TaskStatus.InProgress
+
+    state_machine.publish_mission_status()
+    state_machine.publish_task_status(task=state_machine.current_task)
+
+    resume_mission_response: ControlMissionResponse = (
+        state_machine._make_control_mission_response()
+    )
+    state_machine.queues.resume_mission.output.put(resume_mission_response)
+
+    state_machine.robot.resume()
+    return True
