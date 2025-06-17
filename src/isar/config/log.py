28b8cb91a47e5518d95b45919b08c1ae6322502a
Anke Küstner <AKUST@equinor.com>
2025-06-17 10:04:03 +0200

    Migrate to otel

--- src/isar/config/log.py
+++ src/isar/config/log.py
@@ -3,36 +3,34 @@ import logging.config
 from importlib.resources import as_file, files
 
 import yaml
-from opencensus.ext.azure.log_exporter import AzureLogHandler
 from uvicorn.logging import ColourizedFormatter
 
-from isar.config.configuration_error import ConfigurationError
-from isar.config.keyvault.keyvault_error import KeyvaultError
 from isar.config.keyvault.keyvault_service import Keyvault
 from isar.config.settings import settings
 
 
 def setup_loggers(keyvault: Keyvault) -> None:
     log_levels: dict = settings.LOG_LEVELS
-    source = files("isar").joinpath("config").joinpath("logging.conf")
-    with as_file(source) as f:
-        log_config = yaml.safe_load(open(f))
+    log_config = load_log_config()
 
     logging.config.dictConfig(log_config)
 
     handlers = []
     if settings.LOG_HANDLER_LOCAL_ENABLED:
         handlers.append(configure_console_handler(log_config=log_config))
-    if settings.LOG_HANDLER_APPLICATION_INSIGHTS_ENABLED:
-        handlers.append(
-            configure_azure_handler(log_config=log_config, keyvault=keyvault)
-        )
 
     for log_handler in handlers:
         for loggers in log_config["loggers"].keys():
             logging.getLogger(loggers).addHandler(log_handler)
             logging.getLogger(loggers).setLevel(log_levels[loggers])
         logging.getLogger().addHandler(log_handler)
+
+
+def load_log_config():
+    source = files("isar").joinpath("config").joinpath("logging.conf")
+    with as_file(source) as f:
+        log_config = yaml.safe_load(open(f))
+    return log_config
 
 
 def configure_console_handler(log_config: dict) -> logging.Handler:
@@ -45,23 +43,4 @@ def configure_console_handler(log_config: dict) -> logging.Handler:
             use_colors=True,
         )
     )
-    return handler
-
-
-def configure_azure_handler(log_config: dict, keyvault: Keyvault) -> logging.Handler:
-    connection_string: str
-    try:
-        connection_string = keyvault.get_secret(
-            "application-insights-connection-string"
-        ).value
-    except KeyvaultError:
-        message: str = (
-            "CRITICAL ERROR: Missing connection string for"
-            f" Application Insights in key vault '{keyvault.name}'."
-        )
-        print(f"\n{message} \n")
-        raise ConfigurationError(message)
-
-    handler = AzureLogHandler(connection_string=connection_string)
-    handler.setLevel(log_config["root"]["level"])
     return handler
---
+++ src/isar/config/open_telemetry.py
@@ -0,0 +1,68 @@
+import logging
+
+from azure.identity import ManagedIdentityCredential
+from azure.monitor.opentelemetry.exporter import (
+    AzureMonitorLogExporter,
+    AzureMonitorTraceExporter,
+)
+from fastapi import FastAPI
+from opentelemetry import trace
+from opentelemetry._logs import set_logger_provider
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from isar.config.log import load_log_config
+from isar.config.settings import settings
+
+
+def setup_open_telemetry(app: FastAPI) -> None:
+    if not settings.LOG_HANDLER_APPLICATION_INSIGHTS_ENABLED:
+        return
+    trace_exporter, log_exporter = get_azure_monitor_exporters()
+
+    service_name = settings.OPEN_TELEMETRY_SERVICE_NAME
+    resource = Resource.create({SERVICE_NAME: service_name})
+
+    tracer_provider = TracerProvider(resource=resource)
+    tracer_provider.add_span_processor(BatchSpanProcessor(trace_exporter))
+    trace.set_tracer_provider(tracer_provider)
+
+    log_provider = LoggerProvider(resource=resource)
+    set_logger_provider(log_provider)
+    log_provider.add_log_record_processor(BatchLogRecordProcessor(log_exporter))
+
+    handler = LoggingHandler(logger_provider=log_provider)
+    attach_loggers_for_open_telemetry(handler)
+
+    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer_provider)
+
+
+def attach_loggers_for_open_telemetry(handler: LoggingHandler):
+    log_config = load_log_config()
+
+    for logger_name in log_config["loggers"].keys():
+        logger = logging.getLogger(logger_name)
+        logger.addHandler(handler)
+
+
+def get_azure_monitor_exporters() -> (
+    tuple[AzureMonitorTraceExporter, AzureMonitorLogExporter]
+):
+    """
+    If connection string is defined in environment variables, then use it to create Azure Monitor Exporters.
+    Else use Azure Managed Identity to create Azure Monitor Exporters.
+    """
+    connection_string = settings.APPLICATIONINSIGHTS_CONNECTION_STRING
+    if connection_string:
+        trace_exporter = AzureMonitorTraceExporter(connection_string=connection_string)
+        log_exporter = AzureMonitorLogExporter(connection_string=connection_string)
+    else:
+        credential = ManagedIdentityCredential()
+        trace_exporter = AzureMonitorTraceExporter(credential=credential)
+        log_exporter = AzureMonitorLogExporter(credential=credential)
+
+    return trace_exporter, log_exporter
