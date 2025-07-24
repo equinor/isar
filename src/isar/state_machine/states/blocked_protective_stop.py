c47e37f5d2984ec61f60968b65e8ddc7162c5fa5
Anders Chirico Indrebø <aindreboe@gmail.com>
2025-07-24 12:46:34 +0200

    Refactored offline and blocked_protective_stop

--- src/isar/state_machine/states/blocked_protective_stop.py
+++ src/isar/state_machine/states/blocked_protective_stop.py
@@ -1,24 +1,32 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
-from transitions import State
-
-from isar.state_machine.generic_states.robot_unavailable import (
-    RobotUnavailable,
-    RobotUnavailableStates,
-)
+from isar.eventhandlers.eventhandler import EventHandlerBase, EventHandlerMapping
+from isar.models.communication.queues.events import Event
+from isar.models.communication.queues.queue_utils import check_shared_state
+from robot_interface.models.mission.status import RobotStatus
 
 if TYPE_CHECKING:
     from isar.state_machine.state_machine import StateMachine
 
 
-class BlockedProtectiveStop(State, RobotUnavailable):
-    def __init__(self, state_machine: "StateMachine") -> None:
-        State.__init__(
-            self, name="blocked_protective_stop", on_enter=self.start, on_exit=self.stop
-        )
+def BlockedProtectiveStop(state_machine: "StateMachine"):
+    shared_state = state_machine.shared_state
 
-        RobotUnavailable.__init__(
-            self,
-            state_machine=state_machine,
-            state=RobotUnavailableStates.BlockedProtectiveStop,
-        )
+    def _check_and_handle_robot_status_update(event: Event[RobotStatus]):
+        robot_status: RobotStatus = check_shared_state(event)
+        if robot_status != RobotStatus.BlockedProtectiveStop:
+            return state_machine.robot_status_changed  # type: ignore
+        return None
+
+    event_handlers: List[EventHandlerMapping] = [
+        EventHandlerMapping(
+            name="robot_status_changed_event",
+            eventQueue=shared_state.robot_status,
+            handler=_check_and_handle_robot_status_update,
+        ),
+    ]
+    return EventHandlerBase(
+        state_name="blocked_protective_stop",
+        state_machine=state_machine,
+        event_handler_mappings=event_handlers,
+    )
