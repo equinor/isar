067f9e5603700b0db915d58e52c1734a418a7d79
Anders Chirico Indrebø <aindreboe@gmail.com>
2025-07-25 09:37:51 +0200

    Use new framework for idle states

--- src/isar/state_machine/states/await_next_mission.py
+++ src/isar/state_machine/states/await_next_mission.py
@@ -1,20 +1,59 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
-from transitions import State
+from isar.config.settings import settings
+from isar.eventhandlers.eventhandler import (
+    EventHandlerBase,
+    EventHandlerMapping,
+    TimeoutHandlerMapping,
+)
+from isar.state_machine.utils.generic_event_handlers import (
+    check_and_handle_return_home_event,
+    check_and_handle_start_mission_event,
+    check_and_handle_stop_mission_event,
+)
 
 if TYPE_CHECKING:
     from isar.state_machine.state_machine import StateMachine
 
-from isar.state_machine.generic_states.idle import Idle, IdleStates
 
+def AwaitNextMission(state_machine: "StateMachine"):
+    events = state_machine.events
 
-class AwaitNextMission(State, Idle):
-    def __init__(self, state_machine: "StateMachine") -> None:
-        State.__init__(
-            self, name="await_next_mission", on_enter=self.start, on_exit=self.stop
+    event_handlers: List[EventHandlerMapping] = [
+        EventHandlerMapping(
+            name="start_mission_event",
+            eventQueue=events.api_requests.start_mission.input,
+            handler=lambda event: check_and_handle_start_mission_event(
+                state_machine, event
+            ),
+        ),
+        EventHandlerMapping(
+            name="return_home_event",
+            eventQueue=events.api_requests.return_home.input,
+            handler=lambda event: check_and_handle_return_home_event(
+                state_machine, event
+            ),
+        ),
+        EventHandlerMapping(
+            name="stop_mission_event",
+            eventQueue=events.api_requests.return_home.input,
+            handler=lambda event: check_and_handle_stop_mission_event(
+                state_machine, event
+            ),
+        ),
+    ]
+
+    timers: List[TimeoutHandlerMapping] = [
+        TimeoutHandlerMapping(
+            name="should_return_home_timer",
+            timeout_in_seconds=settings.RETURN_HOME_DELAY,
+            handler=lambda: state_machine.request_return_home,  # type: ignore
         )
-        Idle.__init__(
-            self,
-            state_machine=state_machine,
-            state=IdleStates.AwaitNextMission,
-        )
+    ]
+
+    return EventHandlerBase(
+        state_name="await_next_mission",
+        state_machine=state_machine,
+        event_handler_mappings=event_handlers,
+        timers=timers,
+    )
