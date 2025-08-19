1a13151ca06d9fecec1c400433a832f2ba1a7ca6
oysand <outb@equinor.com>
2025-08-19 09:51:21 +0200

    Add return home retry

--- src/isar/state_machine/transitions/functions/return_home.py
+++ src/isar/state_machine/transitions/functions/return_home.py
@@ -5,8 +5,9 @@ if TYPE_CHECKING:
 
 from typing import TYPE_CHECKING
 
+from isar.config.settings import settings
 from robot_interface.models.mission.mission import Mission
-from robot_interface.models.mission.status import MissionStatus, TaskStatus
+from robot_interface.models.mission.status import MissionStatus, RobotStatus, TaskStatus
 from robot_interface.models.mission.task import ReturnToHome
 
 if TYPE_CHECKING:
@@ -20,6 +21,21 @@ def start_return_home_mission(state_machine: "StateMachine") -> bool:
             name="Return Home",
         )
     )
+    return True
+
+
+def should_retry_return_home(state_machine: "StateMachine") -> bool:
+    if state_machine.shared_state.robot_status.check() != RobotStatus.Available:
+        return False
+
+    return (
+        state_machine.returning_home_state.failed_return_home_attemps
+        < settings.RETURN_HOME_RETRY_LIMIT
+    )
+
+
+def reset_return_home_failure_counter(state_machine: "StateMachine") -> bool:
+    state_machine.returning_home_state.failed_return_home_attemps = 0
     return True
 
 
