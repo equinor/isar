2dc2a0fb8412d89379542514df41eac5d38a0c60
oysand <outb@equinor.com>
2025-09-17 13:39:23 +0200

    Handle battery in seperate thread

--- src/isar/robot/robot_battery.py
+++ src/isar/robot/robot_battery.py
@@ -8,7 +8,7 @@ from robot_interface.models.exceptions.robot_exceptions import RobotException
 from robot_interface.robot_interface import RobotInterface
 
 
-class RobotStatusThread(Thread):
+class RobotBatteryThread(Thread):
     def __init__(
         self,
         robot: RobotInterface,
@@ -19,20 +19,24 @@ class RobotStatusThread(Thread):
         self.shared_state: SharedState = shared_state
         self.robot: RobotInterface = robot
         self.signal_thread_quitting: Event = signal_thread_quitting
-        self.last_robot_status_poll_time: float = (
-            time.time() - settings.ROBOT_API_STATUS_POLL_INTERVAL
-        )
-        Thread.__init__(self, name="Robot status thread")
+        self.last_robot_battery_poll_time: float = time.time()
+        self.force_battery_poll_next_iteration: bool = True
+        Thread.__init__(self, name="Robot battery thread")
 
     def stop(self) -> None:
         return
 
-    def _is_ready_to_poll_for_status(self) -> bool:
-        time_since_last_robot_status_poll = (
-            time.time() - self.last_robot_status_poll_time
+    def _is_ready_to_poll_for_battery(self) -> bool:
+        if self.force_battery_poll_next_iteration:
+            self.force_battery_poll_next_iteration = False
+            return True
+
+        time_since_last_robot_battery_poll = (
+            time.time() - self.last_robot_battery_poll_time
         )
         return (
-            time_since_last_robot_status_poll > settings.ROBOT_API_STATUS_POLL_INTERVAL
+            time_since_last_robot_battery_poll
+            > settings.ROBOT_API_BATTERY_POLL_INTERVAL
         )
 
     def run(self):
@@ -42,16 +46,15 @@ class RobotStatusThread(Thread):
         thread_check_interval = settings.THREAD_CHECK_INTERVAL
 
         while not self.signal_thread_quitting.wait(thread_check_interval):
-            if not self._is_ready_to_poll_for_status():
+            if not self._is_ready_to_poll_for_battery():
                 continue
             try:
-                self.last_robot_status_poll_time = time.time()
+                self.last_robot_battery_poll_time = time.time()
 
-                robot_status = self.robot.robot_status()
+                robot_battery_level = self.robot.get_battery_level()
 
-                self.shared_state.robot_status.update(robot_status)
-
+                self.shared_state.robot_battery_level.update(robot_battery_level)
             except RobotException as e:
-                self.logger.error(f"Failed to retrieve robot status: {e}")
+                self.logger.error(f"Failed to retrieve robot battery level: {e}")
                 continue
-        self.logger.info("Exiting robot status thread")
+        self.logger.info("Exiting robot battery thread")
