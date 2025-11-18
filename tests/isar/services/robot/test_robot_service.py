b8ebdedcaef968891b0b2bf2f9a210a11051356c
Anders Chirico Indrebø <aindreboe@gmail.com>
2025-11-18 15:24:17 +0100

    Add test for successful mission start

--- tests/isar/services/robot/test_robot_service.py
+++ tests/isar/services/robot/test_robot_service.py
@@ -35,3 +35,35 @@ def test_mission_fails_to_schedule(mocked_robot_service: Robot, mocker) -> None:
     assert r_service.monitor_mission_thread is None
 
     mock_publish_mission_status.assert_called_once()
+
+
+def test_mission_succeeds_to_schedule(mocked_robot_service: Robot, mocker) -> None:
+    r_service = mocked_robot_service
+    mocker.patch.object(RobotStartMissionThread, "is_alive", return_value=False)
+    mock_publish_mission_status = mocker.patch(
+        "isar.robot.robot.publish_mission_status"
+    )
+    mock_monitor_mission_start = mocker.patch(
+        "isar.robot.robot.RobotMonitorMissionThread.start"
+    )
+
+    task_1: Task = TakeImage(
+        target=DummyPose.default_pose().position, robot_pose=DummyPose.default_pose()
+    )
+    mission: Mission = Mission(name="Dummy misson", tasks=[task_1])
+
+    r_service.signal_mission_stopped.set()  # We want to test that this is cleared
+
+    r_service.start_mission_thread = RobotStartMissionThread(
+        r_service.robot, r_service.signal_thread_quitting, mission
+    )
+
+    r_service._start_mission_done_handler()
+
+    assert r_service.robot_service_events.mission_started.has_event()
+    assert not r_service.signal_mission_stopped.is_set()
+    assert r_service.monitor_mission_thread is not None
+    assert r_service.monitor_mission_thread.current_mission.id == mission.id
+
+    mock_publish_mission_status.assert_not_called()
+    mock_monitor_mission_start.assert_called_once()
