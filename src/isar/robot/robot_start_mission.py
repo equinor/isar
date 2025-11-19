c3728ef28b609c2daed58716bcc0af294601fd43
Anders Chirico Indrebø <aindreboe@gmail.com>
2025-11-19 09:57:44 +0100

    Avoid retrying to start mission

--- src/isar/robot/robot_start_mission.py
+++ src/isar/robot/robot_start_mission.py
@@ -2,9 +2,9 @@ import logging
 from threading import Event, Thread
 from typing import Optional
 
-from isar.config.settings import settings
 from robot_interface.models.exceptions.robot_exceptions import (
     ErrorMessage,
+    ErrorReason,
     RobotException,
     RobotInfeasibleMissionException,
 )
@@ -27,46 +27,31 @@ class RobotStartMissionThread(Thread):
         Thread.__init__(self, name="Robot start mission thread")
 
     def run(self):
-        retries = 0
+        if self.signal_thread_quitting.wait(0):
+            return
         try:
-            while True:
-                if self.signal_thread_quitting.wait(0):
-                    return
-                try:
-                    self.robot.initiate_mission(self.mission)
-                except RobotInfeasibleMissionException as e:
-                    self.logger.error(
-                        f"Mission is infeasible and cannot be scheduled because: {e.error_description}"
-                    )
-                    self.error_message = ErrorMessage(
-                        error_reason=e.error_reason,
-                        error_description=e.error_description,
-                    )
-
-                    break
-                except RobotException as e:
-                    retries += 1
-                    self.logger.warning(
-                        f"Initiating failed #: {str(retries)} "
-                        f"because: {e.error_description}"
-                    )
-
-                    if retries >= settings.INITIATE_FAILURE_COUNTER_LIMIT:
-                        self.logger.error(
-                            f"Mission will be cancelled after failing to initiate "
-                            f"{settings.INITIATE_FAILURE_COUNTER_LIMIT} times because: "
-                            f"{e.error_description}"
-                        )
-
-                        self.error_message = ErrorMessage(
-                            error_reason=e.error_reason,
-                            error_description=e.error_description,
-                        )
-                        break
-
-                    continue
-                break
+            self.robot.initiate_mission(self.mission)
         except RobotInfeasibleMissionException as e:
+            self.logger.error(
+                f"Mission is infeasible and cannot be scheduled because: {e.error_description}"
+            )
             self.error_message = ErrorMessage(
-                error_reason=e.error_reason, error_description=e.error_description
+                error_reason=e.error_reason,
+                error_description=e.error_description,
+            )
+        except RobotException as e:
+            self.logger.warning(
+                f"Initiating mission failed " f"because: {e.error_description}"
+            )
+            self.error_message = ErrorMessage(
+                error_reason=e.error_reason,
+                error_description=e.error_description,
+            )
+        except Exception as e:
+            self.logger.warning(
+                f"Initiating mission failed due to unknown exception: {e}"
+            )
+            self.error_message = ErrorMessage(
+                error_reason=ErrorReason.RobotUnknownErrorException,
+                error_description=str(e),
             )
