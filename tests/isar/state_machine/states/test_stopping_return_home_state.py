e344430f023d323f03c5e7c7a706d0fa20bac1e6
andchiind <aindreboe@gmail.com>
2025-11-25 14:19:58 +0100

    Respond quickly when stopping return home

--- tests/isar/state_machine/states/test_stopping_return_home_state.py
+++ tests/isar/state_machine/states/test_stopping_return_home_state.py
@@ -2,6 +2,8 @@ from typing import Optional, cast
 
 from isar.eventhandlers.eventhandler import EventHandlerBase, EventHandlerMapping
 from isar.state_machine.state_machine import StateMachine
+from robot_interface.models.mission.mission import Mission
+from tests.test_mocks.task import StubTask
 
 
 def test_return_home_cancelled_when_new_mission_received(
@@ -17,9 +19,11 @@ def test_return_home_cancelled_when_new_mission_received(
         returning_home_state.get_event_handler_by_name("start_mission_event")
     )
 
+    mission: Mission = Mission(name="Dummy misson", tasks=[StubTask.take_image()])
+
     assert event_handler is not None
 
-    event_handler.event.trigger_event(True)
+    event_handler.event.trigger_event(mission)
     transition = event_handler.handler(event_handler.event)
 
     assert transition is sync_state_machine.stop_return_home  # type: ignore
