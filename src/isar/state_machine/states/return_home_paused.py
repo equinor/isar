8f151b4aebbfe2ba26aa766ce6dc6d1c72ceb4b2
andchiind <aindreboe@gmail.com>
2025-12-17 13:36:27 +0100

    Reply to resume request quickly

--- src/isar/state_machine/states/return_home_paused.py
+++ src/isar/state_machine/states/return_home_paused.py
@@ -1,6 +1,10 @@
 from typing import TYPE_CHECKING, Callable, List, Optional
 
-from isar.apis.models.models import LockdownResponse, MissionStartResponse
+from isar.apis.models.models import (
+    ControlMissionResponse,
+    LockdownResponse,
+    MissionStartResponse,
+)
 from isar.config.settings import settings
 from isar.eventhandlers.eventhandler import EventHandlerBase, EventHandlerMapping
 from isar.models.events import Event
@@ -77,6 +81,9 @@ class ReturnHomePaused(EventHandlerBase):
 
         def _resume_mission_event_handler(event: Event[bool]):
             if event.consume_event():
+                state_machine.events.api_requests.resume_mission.response.trigger_event(
+                    ControlMissionResponse(success=True)
+                )
                 state_machine.events.state_machine_events.resume_mission.trigger_event(
                     True
                 )
