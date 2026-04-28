5e5e58b519822c04e10cff6323bd3b4bbc939714
Anders C Indrebø <aindreboe@gmail.com>
2026-04-28 12:59:41 +0200

    Add test for stopping mission when recharging

--- tests/isar/state_machine/states/test_going_to_recharging_state.py
+++ tests/isar/state_machine/states/test_going_to_recharging_state.py
@@ -64,3 +64,22 @@ def test_return_home_goes_to_recharging_when_battery_low(
 
     sync_state_machine.current_state = transition(sync_state_machine)
     assert type(sync_state_machine.current_state) is GoingToRecharging
+
+
+def test_cancelling_mission_when_going_home_to_recharge(
+    sync_state_machine: StateMachine,
+) -> None:
+    sync_state_machine.current_state = GoingToRechargingWithMission(
+        sync_state_machine, mission=AbortedMission(name="test", id="test_id")
+    )
+    returning_home_state: State = cast(State, sync_state_machine.current_state)
+    event_handler: EventHandlerMapping | None = (
+        returning_home_state.get_event_handler_by_name("stop_mission_event")
+    )
+
+    assert event_handler is not None
+
+    transition = event_handler.handler("test_id")
+
+    sync_state_machine.current_state = transition(sync_state_machine)
+    assert type(sync_state_machine.current_state) is GoingToRecharging
